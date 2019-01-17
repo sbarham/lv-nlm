e59b304a6fa29342e83e0b453a43aec3e8c8321c
Samuel Barham <sbarham@terpmail.umd.edu>
2019-01-17 22:07:52 +0000

    Committing new util/ directory

---
+++ util/__init__.py
@@ -0,0 +1,13 @@
+DATA_DIR = '../data'
+PTB_DIR = 'penn'
+BROWN_DIR = 'brown'
+GUTENBERG_DIR = 'gutenberg'
+BIBLE_DIR = 'bible'
+WIKITEXT2_DIR = 'wikitext-2'
+WIKITEXT103_DIR = 'wikitext-103'
+
+TRAIN = 'train'
+TEST = 'test'
+VAL = 'val'
+
+MODEL_DIR = 'bin'
